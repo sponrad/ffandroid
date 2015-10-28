2ab6e9ea5c581655861474e534d3297d29d37371
Conrad <sponrad@gmail.com>
2015-10-28 10:43:24 -0700

    change background color test

--- app/src/main/java/com/flashforceapp/www/ffandroid/FlashActivity.java
+++ app/src/main/java/com/flashforceapp/www/ffandroid/FlashActivity.java
@@ -1,9 +1,11 @@
 package com.flashforceapp.www.ffandroid;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class FlashActivity extends AppCompatActivity {
 
@@ -11,6 +13,17 @@ public class FlashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
+
+        // Now get a handle to any View contained
+        // within the main layout you are using
+        View someView = findViewById(R.id.flash_handler);
+
+        // Find the root view
+        View root = someView.getRootView();
+
+        // Set the color
+        root.getBackground().setColorFilter(Color.parseColor("#0000FF"));
+        //root.setBackgroundColor(getResources().getColor(android.R.color.red));
     }
 
     @Override
