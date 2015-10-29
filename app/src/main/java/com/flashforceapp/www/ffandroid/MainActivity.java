314f0c798447a2e4018f5aaaf465c307754b3fb0
Conrad <sponrad@gmail.com>
2015-10-29 13:00:45 -0700

    potential database classes added

--- app/src/main/java/com/flashforceapp/www/ffandroid/MainActivity.java
+++ app/src/main/java/com/flashforceapp/www/ffandroid/MainActivity.java
@@ -1,6 +1,10 @@
 package com.flashforceapp.www.ffandroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +21,8 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -24,6 +30,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     public double avgOffset = 0.0;
+    public boolean ffdbLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             firstTimeBoot()  //get owned IAPs and show tutorial images
         }
          */
+        //new RetrieveFeedTask().execute();
     }
 
     @Override
@@ -90,60 +98,129 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-
-        private Exception exception;
-
-        protected void onPreExecute() {
-            //progressBar.setVisibility(View.VISIBLE);
-            //responseView.setText("");
+    public void checkDatabase(){
+        if (ffdbLoaded == false) {
+            loadDatabase();
         }
-
-        protected String doInBackground(Void... urls) {
-            //String email = emailText.getText().toString();
-            // Do some validation here
-
-            try {
-                URL url = new URL("https://alignthebeat.appspot.com");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
-            }
-            //progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
-            //responseView.setText(response);
-
-            try {
-                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                String requestID = object.getString("requestId");
-                int likelihood = object.getInt("likelihood");
-                JSONArray photos = object.getJSONArray("photos");
-
-            } catch (JSONException e) {
-                // Appropriate error handling code
-            }
+        else {
         }
     }
 
+    public void loadDatabase(){
+        String mCSVfile = "file.csv";
+        AssetManager manager = context.getAssets();
+        InputStream inStream = null;
+        try {
+            inStream = manager.open(mCSVfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(inStream));
+
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(inStream));
+        String line = "";
+        db.beginTransaction();
+        try {
+            while ((line = buffer.readLine()) != null) {
+                String[] colums = line.split(",");
+                if (colums.length != 4) {
+                    Log.d("CSVParser", "Skipping Bad CSV Row");
+                    continue;
+                }
+                ContentValues cv = new ContentValues(3);
+                cv.put(dbCol0, colums[0].trim());
+                cv.put(dbCol1, colums[1].trim());
+                cv.put(dbCol2, colums[2].trim());
+                cv.put(dbCol3, colums[3].trim());
+                cv.put(dbCol4, colums[4].trim());
+                db.insert(TABLE, null, cv);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        db.setTransactionSuccessful();
+        db.endTransaction();
+    }
+
+
+}
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DICTIONARY_TABLE_NAME = "dictionary";
+    private static final String DICTIONARY_TABLE_CREATE =
+            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
+                    KEY_WORD + " TEXT, " +
+                    KEY_DEFINITION + " TEXT);";
+
+    DictionaryOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DICTIONARY_TABLE_CREATE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+}
+
+
+class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+    private Exception exception;
+
+    protected void onPreExecute() {
+        //progressBar.setVisibility(View.VISIBLE);
+        //responseView.setText("");
+    }
+
+    protected String doInBackground(Void... urls) {
+        //String email = emailText.getText().toString();
+        // Do some validation here
+
+        try {
+            URL url = new URL("https://alignthebeat.appspot.com");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        //progressBar.setVisibility(View.GONE);
+        Log.i("INFO", response);
+        //responseView.setText(response);
+
+        try {
+            JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+            String requestID = object.getString("requestId");
+            int likelihood = object.getInt("likelihood");
+            JSONArray photos = object.getJSONArray("photos");
+
+        } catch (JSONException e) {
+            // Appropriate error handling code
+        }
+    }
 }
