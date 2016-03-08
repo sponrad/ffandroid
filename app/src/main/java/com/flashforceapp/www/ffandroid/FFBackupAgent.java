0c27df0b49fdcd40e407eaad088ddf3a18221e68
Conrad <sponrad@gmail.com>
2016-03-08 12:24:00 -0800

    hardcode backup agent string references

--- app/src/main/java/com/flashforceapp/www/ffandroid/FFBackupAgent.java
+++ app/src/main/java/com/flashforceapp/www/ffandroid/FFBackupAgent.java
@@ -8,10 +8,10 @@ import android.app.backup.SharedPreferencesBackupHelper;
  */
 public class FFBackupAgent extends BackupAgentHelper {
     // The name of the SharedPreferences file
-    String PREFS = getString(R.string.userpref);
+    String PREFS = "com.flashforceapp.www.ffandroid.userPrefString";
 
     // A key to uniquely identify the set of backup data
-    String PREFS_BACKUP_KEY = getString(R.string.prefkey);
+    String PREFS_BACKUP_KEY = "PrefKeycom.flashforceapp.www.ffandroid.userpref";
 
     // Allocate a helper and add it to the backup agent
     @Override
