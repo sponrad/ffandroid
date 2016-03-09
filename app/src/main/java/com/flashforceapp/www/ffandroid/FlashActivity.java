90cc5408c12657766b0023bbdc1a21d8706b7542
Conrad <sponrad@gmail.com>
2016-03-08 20:53:43 -0800

    remove bottom buttons

--- app/src/main/java/com/flashforceapp/www/ffandroid/FlashActivity.java
+++ app/src/main/java/com/flashforceapp/www/ffandroid/FlashActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import java.util.ArrayList;
@@ -33,6 +34,9 @@ public class FlashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);  //does not work
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         Log.i("INFO", "started FlashActivity");
         setContentView(R.layout.activity_flash);
